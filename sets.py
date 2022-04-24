b7bd6245ff563b4bedafd89caa54b0604b71951e
abzx24819s <103437196+abzx24819s@users.noreply.github.com>
2022-04-24 18:25:29 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -4,20 +4,20 @@ def union(l1,l2):
     ul=l1+l2
     ul=set(ul)
     ul=list(ul)
-    return print("union of l1 and l2 is",ul)
+    return print("union of l1 and l2 is:",ul)
 def intersection(l1,l2):
     for i in range(len(l1)):
         il=set(l1)&set(l2)
         il=list(il)
-    return print("intersection of l1 and l2 is",il)
+    return print("intersection of l1 and l2 is:",il)
 def difference(l1,l2):
     dl=list(set(l1)-set(l2))+list(set(l2)-set(l1))
-    return print("difference of l1 and l2 is",dl)
+    return print("difference of l1 and l2 is:",dl)
 def symdiff(l1,l2):
     a=set(l1)
     b=set(l2)
     sdl=list(a.symmetric_difference(b))
-    return print("symmetrical difference of l1 and l2 is",sdl)
+    return print("symmetrical difference of l1 and l2 is:",sdl)
 union(l1,l2)
 intersection(l1,l2)
 difference(l1,l2)
