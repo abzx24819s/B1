698cd3141b76bd1c378a8026d292489bacd7d4cd
abzx24819s <103437196+abzx24819s@users.noreply.github.com>
2022-04-24 18:46:50 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+list1 = [12, -7, 5, 64, -14]
+for i in range(len(list1)):
+    if (list1[i]>=0):
+        print(list1[i],end=" ")
+list2 = [12, 14, -95, 3]
+print("\n")
+for num in list2:
+    if num>=0:
+        print(num,end=" ")
